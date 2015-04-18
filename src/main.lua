5e19da2d0e7e452370db1a02db112343a5436828
Paul Liverman <paul.liverman.iii@gmail.com>
2015-04-17 21:00:30 -0700

    moving, tiling, editing

--- src/main.lua
+++ src/main.lua
@@ -1,7 +1,9 @@
 local inspect = require "lib.inspect"
 local serialize = require "lib.ser"
+local cron = require "lib.cron"
 
 local lg = love.graphics
+local lk = love.keyboard
 
 local tiles, tileset -- loaded in love.load
 
@@ -9,8 +11,8 @@ local scale = 2
 local tileSize = 16 * scale
 
 local player = {
-    x = 1,
-    y = 1,
+    x = 9,
+    y = 7,
     image = false --loaded after fixing image filter
 }
 
@@ -29,8 +31,8 @@ function love.load()
 end
 
 function love.draw()
-    for x=camera.x,15 do
-        for y=camera.y,10 do
+    for x=camera.x,camera.x+14 do
+        for y=camera.y,camera.y+9 do
             if tiles[x] and tiles[x][y] then
                 lg.draw(tileset[tiles[x][y]], (x - camera.x) * tileSize, (y - camera.y) * tileSize, 0, scale, scale)
             end
@@ -40,23 +42,75 @@ function love.draw()
     lg.draw(player.image, (player.x - camera.x) * tileSize, (player.y - camera.y) * tileSize, 0, scale, scale)
 end
 
-function love.keypressed(key, unicode)
-    if key == "w" then
+local movement
+local function move()
+    local moved = false
+
+    if lk.isDown("w") then
         player.y = player.y - 1
-    elseif key == "a" then
+        moved = true
+    elseif lk.isDown("a") then
         player.x = player.x - 1
-    elseif key == "s" then
+        moved = true
+    elseif lk.isDown("s") then
         player.y = player.y + 1
-    elseif key == "d" then
+        moved = true
+    elseif lk.isDown("d") then
         player.x = player.x + 1
-    elseif key == "escape" then
+        moved = true
+    end
+
+    if moved then
+        if math.abs(player.x - camera.x) < 5 then
+            camera.x = camera.x - 1
+        elseif math.abs(player.x - camera.x) > 9 then
+            camera.x = camera.x + 1
+        elseif math.abs(player.y - camera.y) < 3 then
+            camera.y = camera.y - 1
+        elseif math.abs(player.y - camera.y) > 6 then
+            camera.y = camera.y + 1
+        end
+
+        movement = cron.after(1/6, move)
+    end
+end
+
+function love.update(dt)
+    if movement then movement:update(dt) end
+end
+
+function love.keypressed(key, unicode)
+    if key == "escape" then
         love.event.quit()
+    end
+
+    move()
+end
+
+local function maketile(x, y)
+    if not tiles[x] then
+        tiles[x] = {}
+        if x < tiles.minX then
+            tiles.minX = x
+        elseif x > tiles.maxX then
+            tiles.maxX = x
+        end
+    end
+    if not tiles[x][y] then
+        tiles[x][y] = 0
+        if y < tiles.minY then
+            tiles.minY = y
+        elseif y > tiles.maxY then
+            tiles.maxY = y
+        end
     end
 end
 
 function love.mousepressed(x, y, button)
-    x = math.floor(x / tileSize) + 1
-    y = math.floor(y / tileSize) + 1
+    x = math.floor(x / tileSize) + camera.x
+    y = math.floor(y / tileSize) + camera.y
+
+    maketile(x, y)
 
     if button == "wu" then
         tiles[x][y] = tiles[x][y] - 1
@@ -68,9 +122,31 @@ function love.mousepressed(x, y, button)
         if tiles[x][y] > tileset.count then
             tiles[x][y] = 0
         end
+    elseif button == "r" then
+        selectedTile = tiles[x][y]
+    elseif button == "l" then
+        tiles[x][y] = selectedTile
     end
 end
 
+local next = next
+
 function love.quit()
+    for x=tiles.minX,tiles.maxX do
+        for y=tiles.minY,tiles.maxY do
+            if tiles[x] and tiles[x][y] == 0 then
+                tiles[x][y] = nil
+            end
+        end
+        if next(tiles[x]) == nil then
+            tiles[x] = nil
+            if x == tiles.minX then
+                tiles.minX = x + 1
+            end
+        end
+    end
+    tiles.maxX = #tiles
+    collectgarbage()
+    -- dunno how to check / set minY and maxY (well, I do, but it would be time-consuming and boring)
     love.filesystem.write("tiles.lua", serialize(tiles))
 end
